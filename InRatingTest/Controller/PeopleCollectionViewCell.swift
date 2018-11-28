d8c9e6121a4e7dd0bcdd77a681a8749771abc608
Dareniar <DanilShchegol@icloud.com>
2018-11-28 19:09:01 +0200

    UI has been completed.

---
+++ InRatingTest/Controller/PeopleCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PeopleCollectionViewCell.swift
+//  InRatingTest
+//
+//  Created by Данил on 11/28/18.
+//  Copyright © 2018 Dareniar. All rights reserved.
+//
+
+import UIKit
+
+class PeopleCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ InRatingTest/Model/People.swift
@@ -0,0 +1,16 @@
+//
+//  People.swift
+//  InRatingTest
+//
+//  Created by Данил on 11/28/18.
+//  Copyright © 2018 Dareniar. All rights reserved.
+//
+
+import Foundation
+
+struct People {
+    
+    let name: String
+    let avatar: String
+    
+}
